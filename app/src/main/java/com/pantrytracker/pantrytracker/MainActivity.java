9556cb952bd5da50b4e5ba372cdd0d5e9ad3d739
Divinee Chidume <divineechidume@berkeley.edu>
2018-12-26 03:29:59 -0800

    added a second activity to the program:

--- app/src/main/java/com/pantrytracker/pantrytracker/MainActivity.java
+++ app/src/main/java/com/pantrytracker/pantrytracker/MainActivity.java
@@ -2,12 +2,34 @@ package com.pantrytracker.pantrytracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+    private Button inside;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialize the button to get to the inside of the pantry
+        inside = findViewById(R.id.check_pantry);
+        inside.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                food_locations();
+            }
+        });
     }
+
+    /**
+     * This is where we open the pantry and the use can see the next options."
+     */
+    public void food_locations() {
+        Intent intent = new Intent(this, food_locations.class);
+        startActivity(intent);
+    }
+
 }

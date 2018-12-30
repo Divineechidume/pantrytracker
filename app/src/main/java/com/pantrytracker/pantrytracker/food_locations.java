0cea9b136107618def7ba0ac9a2fc0fa2a1a1dfd
Divinee Chidume <divineechidume@berkeley.edu>
2018-12-30 04:56:55 -0800

    I justed added a settings page and am trying to add a recyclerview

--- app/src/main/java/com/pantrytracker/pantrytracker/food_locations.java
+++ app/src/main/java/com/pantrytracker/pantrytracker/food_locations.java
@@ -2,12 +2,45 @@ package com.pantrytracker.pantrytracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class food_locations extends AppCompatActivity {
+    private Button settings;
+    private Button fridge;
+    private Button analytics;
+    private Button pantry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_locations);
+
+        settings = findViewById(R.id.settings);
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SettingsActivity();
+            }
+        });
+
+        pantry = findViewById(R.id.pantry);
+        pantry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pantryactivity();
+            }
+        });
+    }
+
+    public void SettingsActivity() {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
+    public void pantryactivity() {
+        Intent intent = new Intent(this, pantry.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/pantrytracker/pantrytracker/pantry.java
@@ -0,0 +1,13 @@
+package com.pantrytracker.pantrytracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pantry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantry);
+    }
+}

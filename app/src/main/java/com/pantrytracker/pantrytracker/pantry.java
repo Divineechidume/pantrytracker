29293f31c856ff45ccd6dd683a105ebd7004da41
Divineechidume <divineechidume@berkeley.edu>
2020-04-17 19:00:28 -0700

    made some updates that I forgot to push

--- app/src/main/java/com/pantrytracker/pantrytracker/pantry.java
+++ app/src/main/java/com/pantrytracker/pantrytracker/pantry.java
@@ -1,13 +1,34 @@
 package com.pantrytracker.pantrytracker;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.ArrayList;
+import android.widget.ArrayAdapter;
+import android.view.View;
 
-public class pantry extends AppCompatActivity {
+public class pantry extends ListActivity {
+
+    ArrayList<String> listItems=new ArrayList<String>();
+
+    //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
+    ArrayAdapter<String> adapter;
+
+    //RECORDING HOW MANY TIMES THE BUTTON HAS BEEN CLICKED
+    int clickCounter=0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
         setContentView(R.layout.activity_pantry);
+        adapter=new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listItems);
+        setListAdapter(adapter);
+    }
+
+    //METHOD WHICH WILL HANDLE DYNAMIC INSERTION
+    public void addItems(View v) {
+        adapter.add("New Item");
     }
 }
